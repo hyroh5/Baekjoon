fc419de756f30afc0294bc1bd643318f11ef54bc
heeyouneee <hyrlove5598@khu.ac.kr>
2025-06-30 18:11:48 +0900

    [Silver V] Title: 수들의 합 5, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -2,10 +2,10 @@ N = int(input())
 count = 0
 
 for k in range(1, N+1):
-  validation = (2*N)/k-k+1   #validation = 2a
-  if validation <= 0:    #2N/k 에서 결론적으로 부동소수점 오차 발생해서 의미 없음
+  validation = 2*N - k*(k-1)  #validation = 2ak
+  if validation <= 0:
     break
-  if validation % 2 == 0:
+  if validation % (2*k) == 0:
     count += 1
 
 print(count)
