8a254f359a0867d6a4b513ff75451c490412dcd4
heeyouneee <hyrlove5598@khu.ac.kr>
2025-09-30 11:32:20 +0900

    [Silver V] Title: LCM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+ long long gcd(long long a, long long b) {
+  if (a%b == 0) return b;
+  return gcd(b, a%b);
+}
+
+long long lcm(long long a, long long b) {
+  return a*b / gcd(a, b);
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  for (int i=0; i<n; i++) {
+    int a, b;
+    cin >> a >> b;
+    cout << lcm(a, b) << endl;
+  }
+
+  return 0;
+}
